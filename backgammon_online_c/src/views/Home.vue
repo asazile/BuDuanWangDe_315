551fd7c19c278cc7e638dc9c5b846dac3b5844f0
daoerche <daoerche@qq.com>
2018-05-26 16:57:40 +0800

    import Element

--- backgammon_online_c/src/views/Home.vue
+++ backgammon_online_c/src/views/Home.vue
@@ -1,7 +1,12 @@
 <template>
   <div class="home">
-    <img src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <el-container>
+      <el-header>Header</el-header>
+      <el-container>
+        <el-aside width="200px">Aside</el-aside>
+        <el-main>Main</el-main>
+      </el-container>
+    </el-container>
   </div>
 </template>
 
