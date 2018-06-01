11c3997affe01866ccdc4ab76e0ad9d755486fea
daoerche <daoerche@qq.com>
2018-06-01 22:38:46 +0800

    import express-session.

--- backgammon_online_s/app.js
+++ backgammon_online_s/app.js
@@ -4,6 +4,7 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+const session = require('express-session');
 
 var index = require('./routes/index');
 var users = require('./routes/users');
@@ -21,6 +22,13 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(session({
+    secret: 'bu duan wang de 315',
+    cookie: { maxAge: 1000 * 60 * 60 * 60},
+    resave: true,
+    saveUninitialized: true
+}));
 
 app.use('/', index);
 app.use('/users', users);
