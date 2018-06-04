59db87db41585d592697a5b6ed98ba74909cc229
flyvv <1003553948@qq.com>
2018-06-04 22:26:39 +0800

    xiu gai mysql

--- backgammon_online_s/mysql/users.sql
+++ backgammon_online_s/mysql/users.sql
@@ -1,9 +1,9 @@
-CREATE DATABASE buduanwangde315;
+CREATE DATABASE buduanwangde315;		/*创建buduanwangde315数据库*/
 
-CREATE TABLE IF NOT EXISTS `user` (
-    id SMALLINT UNSIGNED KEY AUTO_INCREMENT,
-    username VARCHAR(32) UNIQUE NOT NULL,
-    name VARCHAR(32) NOT NULL,
-    password VARCHAR(32) NOT NULL,
-    rank SMALLINT UNSIGNED NOT NULL DEFAULT 0
+CREATE TABLE IF NOT EXISTS `user` (   		/*创建user表*/
+    id SMALLINT UNSIGNED KEY AUTO_INCREMENT,   /*id用无符号正数，id自增加*/
+    username VARCHAR(32) UNIQUE NOT NULL,	/*用户名唯一*/
+    name VARCHAR(32) NOT NULL,          /*昵称*/
+    password VARCHAR(32) NOT NULL,	/*密码*/
+    rank SMALLINT UNSIGNED NOT NULL DEFAULT 0  /*排名 分 */
 )ENGINE=INNODB CHARSET=UTF8;
